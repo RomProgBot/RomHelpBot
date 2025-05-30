725178c1dd2030989300652430c70acfae4dc0ff
RomProgBot <RomanRudskoySlava@gmail.com>
2025-05-31 05:32:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 # Ваш токен Telegram бота
-BOT_TOKEN = '8150614057:AAF6qM6JXUsTpAs7-g-pGI7zIVdLXCu6k4Y'
+BOT_TOKEN = '8150614057:AAFQBLhyfngDmactCk1ize_QT3imIX1L4l0'
 
 
 
